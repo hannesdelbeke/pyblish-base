d9ad19807025818719466948fea1f9f835d2accc
hannes.delbeke <hannesdelbeke@gmail.com>
2022-01-23 22:36:45 +0000

    actions from module

--- pyblish/plugin.py
+++ pyblish/plugin.py
@@ -1398,6 +1398,52 @@ def discover(type=None, regex=None, paths=None):
     return plugins
 
 
+def actions_from_module(module):
+    """Return actions from module
+
+    Arguments:
+        module (types.ModuleType): Imported module from which to
+            parse valid Pyblish plug-ins.
+
+    Returns:
+        List of plug-ins, or empty list if none is found.
+
+    """
+
+    actions = list()
+
+    for name in dir(module):
+        if name.startswith("_"):
+            continue
+
+        # It could be anything at this point
+        obj = getattr(module, name)
+
+        if not inspect.isclass(obj):
+            continue
+
+        if not issubclass(obj, Action):
+            continue
+
+        # if not plugin_is_valid(obj):
+        #     log.debug("Plug-in invalid: %s", obj)
+        #     continue
+
+        # if not version_is_compatible(obj):
+        #     log.debug("Plug-in %s not compatible with "
+        #               "this version (%s) of Pyblish." % (
+        #                   obj, __version__))
+        #     continue
+
+        # if not host_is_compatible(obj):
+        #     log.debug("No supported host found for plugin:%s",  obj)
+        #     continue
+
+        actions.append(obj)
+
+    return actions
+
+
 def plugins_from_module(module):
     """Return plug-ins from module
 
